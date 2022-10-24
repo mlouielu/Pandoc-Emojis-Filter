19cf7a7cd099fc4f994bec7c8c408792871e47c1
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-24 10:28:23 +0800

    Delete create-example.sh

--- create-example.sh
+++
@@ -1,5 +0,0 @@
-format=pdf
-npm install
-pandoc --template="template.tex" -o example.$format readme.md \
-    --filter=emoji_filter.js -M emoji=noto-emoji --from gfm \
-    -V links-as-notes=true -V colorlinks -V urlcolor=NavyBlue
